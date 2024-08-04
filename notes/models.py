ec743f2cb9f46b7216945cbbf4a39fce2c08c09a
ujjawal <ujjawal17032002@gmail.com>
2024-08-04 16:38:03 +0530

    add is_view field

--- notes/models.py
+++ notes/models.py
@@ -2,14 +2,19 @@ from django.db import models
 import uuid
 # Create your models here.
 # NOTE: remove the blank and null property from the scheme.
+# NOTE: how to upload the multiple images.
 
 
 class Notes(models.Model):
     title = models.CharField(max_length=30, null=True, blank=True)
     body = models.TextField(blank=True)
     images = models.ImageField(upload_to="notes/images/", null=True, blank=True)
+    is_viewd = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
+
+    def __str__(self):
+        return str(self.title)
