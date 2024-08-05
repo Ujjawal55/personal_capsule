ddd5082d4f22ab252c89bebbd9b355c264c1a95f
ujjawal <ujjawal17032002@gmail.com>
2024-08-05 18:05:41 +0530

    change the url for edit and delete

--- notes/urls.py
+++ notes/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.notesListView, name="notes-list"),
     path("create/", views.createNotesView, name="create-notes"),  # type: ignore
-    path("edit/<str:pk>", views.editNotesView, name="edit-notes"),
-    path("delete/<str:pk>", views.deleteNotesView, name="delete-notes"),
     path("<str:pk>/", views.notesDetailView, name="notes-detail"),
+    path("<str:pk>/edit", views.editNotesView, name="edit-notes"),
+    path("<str:pk>/delete", views.deleteNotesView, name="delete-notes"),
     path("<str:pk>/image/", views.imageView, name="view-image"),
 ]
