c6371f611e732000befe28479f1b529632b72175
ujjawal <ujjawal17032002@gmail.com>
2024-08-06 14:15:31 +0530

    add bookmark

--- study_resources/urls.py
+++ study_resources/urls.py
@@ -4,4 +4,7 @@ from study_resources import views
 
 app_name = "study-resources"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.resourcesListView, name="resources-list"),
+    path("bookmark/", views.BookmarkView, name="bookmark"),
+]
--- study_resources/views.py
+++ study_resources/views.py
@@ -1,2 +1,14 @@
 from django.shortcuts import render, redirect
+from .forms import BookmarkForm
 # Create your views here.
+
+
+def resourcesListView(request):
+    return render(request, "study_resources/resources_list.html")
+
+
+def BookmarkView(request):
+    form = BookmarkForm()
+
+    context = {"form": form}
+    return render(request, "study_resources/bookmark.html", context)
