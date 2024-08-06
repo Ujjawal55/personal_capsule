96e9193b591f5762765fe7033dc4a1a2e187d5b1
ujjawal <ujjawal17032002@gmail.com>
2024-08-06 10:14:59 +0530

    add resourcelist modes

--- study_resources/models.py
+++ study_resources/models.py
@@ -1,3 +1,11 @@
+from os import walk
 from django.db import models
 
 # Create your models here.
+
+
+class ResourcesList(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.name)
