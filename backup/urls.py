786c148ab3a3413a181e2f6c625768881aed1405
ujjawal <ujjawal17032002@gmail.com>
2024-08-06 15:46:55 +0530

    update model.py

---
+++ backup/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from study_resources import views
+
+
+app_name = "study-resources"
+
+urlpatterns = [
+    path("", views.resourcesListView, name="resources-list"),
+    path("bookmark/", views.BookmarkView, name="bookmarks-list"),
+    path("bookmark/create/", views.createBookmarkView, name="create-bookmark"),
+    path("bookmark/edit/<str:pk>", views.editBookmarkView, name="edit-bookmark"),
+]
