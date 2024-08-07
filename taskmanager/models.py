6eb892649e397796793ba5ce0ac0787423c09b25
ujjawal <ujjawal17032002@gmail.com>
2024-08-07 16:18:14 +0530

    add models.py

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class DailyTask(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return str(self.title)
+
+
+class SubTask(models.Model):
+    daily_task = models.ForeignKey(
+        DailyTask, on_delete=models.CASCADE, related_name="subtasks"
+    )
+    title = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return str(self.title)
